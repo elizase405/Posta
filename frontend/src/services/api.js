84571415bddd5fca233d86382c31fd6f5a526301
elizase405 <elizase405@gmail.com>
2024-05-31 10:49:38 +0100

    feat: Add live backend server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:5000/'
+const baseUrl = 'https://posta-qwxv.onrender.com/'
 const auth = baseUrl + 'api/auth'
 const api = baseUrl + 'api/recipes' 
 
